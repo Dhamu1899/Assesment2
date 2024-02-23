986176d4e4e3b073e9d243c82d171df4812df381
Dhamu1899 <149763926+Dhamu1899@users.noreply.github.com>
2024-02-23 17:03:50 +0530

    Add files via upload

---
+++ as2.sql
@@ -0,0 +1,28 @@
+-- CREATE TABLE Course (
+--     course_id INT PRIMARY KEY,
+--     course_name VARCHAR(255),
+--     fees INT
+-- );
+
+-- CREATE TABLE Student (
+--     stud_id INT PRIMARY KEY,
+--     stud_name VARCHAR(255),
+--     qualification VARCHAR(50),
+--     email VARCHAR(255),
+--     contact BIGINT,
+--     course_id INT,
+--     date_of_joining DATE
+-- );
+
+-- INSERT INTO Course VALUES (1, 'FSD', 5000);
+-- INSERT INTO Course VALUES (2, 'PGA', 6000);
+-- INSERT INTO Course VALUES (3, 'Other', 4500);
+
+-- INSERT INTO Student VALUES (1, 'Fathima', 'MCA', 'fathima@gmail.com', 9876543210, 1, '2024-02-12');
+-- INSERT INTO Student VALUES (2, 'Raveena', 'BSc', 'raveena@gmail.com', 8776543219, 2, '2024-02-20');
+-- -- Add other records similarly
+
+--select * from course
+
+select * from Student
+
